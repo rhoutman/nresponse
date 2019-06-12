9c42b4a323058afbda7aa4ae914063a0f11bb5af
Rene Houtman <rene_houtman@hotmail.com>
2019-06-12 15:43:22 +0000

    cleaned up

--- main.R
+++ main.R
@@ -11,26 +11,52 @@ input <- list(
   conf.level = 0.95,
   abase = 10,
   do.ttest = T,
-  NC.factor = ctx$colors[[1]],
-  NC.annotation = "DMSO",
-  value = ".y",
   Ignore.negatives = as.logical(ctx$op.value('ignore negatives'))
 )
 
-ctxcore <- ctx %>% 
+# extract the data
+ctxcore <- ctx %>%  
   select() 
 
+# reformat column names for odd characters
+colnames(ctxcore) <- colnames(ctxcore) %>% make.names()
+
+# create a column for negative control annotation
+if(ctx$labels %>% length() <1){
+  stop("Drag the annotation factor for the negative control (NC) to 'Labels' first! \n Next, provide the annotion for the NC in the operator properties.")
+}else{
+  ctxcore$NCfactor = ctxcore[[ctx$labels[[1]] %>% make.names()]]
+}
+
+# create a column to split the data (if present)
+if(ctx$colors %>% length() >0) {
+  asplit <- ctx$colors %>% as.character() 
+  ctxcore <- ctxcore %>% 
+    unite("split" ,asplit, remove=F)
+} else{ctxcore$split = ""} 
+
+# filter for needed data
+ctxcore <- ctxcore %>% 
+  select(.ri, 
+         .ci, 
+         NCfactor,
+         split,
+         .y)
+
+# if indicated in properties, convert negatives values to small positive
 if(as.logical(ctx$op.value('convert negatives'))){
   ctxcore$.y <- with(ctxcore, replace(.y, .y<1, 1))
 }
 
+
+# ttest function  
 ttest <- function(pop1, pop2, input) {
   alternative <- input$alternative
   mu <- input$mu
   paired <- input$paired
   var.equal <- input$var.equal
   conf.level <- input$conf.level
-
+  
   ds <- try(t.test(pop1, pop2, alternative, mu, paired, var.equal, conf.level))
   p <- if (!inherits(ds, "try-error")) {
     ds$p.value
@@ -40,9 +66,10 @@ ttest <- function(pop1, pop2, input) {
   return(p)
 }
 
+# perform some basic calculations
 dostats <- function(df, pop1, input) {
   abase <- input$abase
-  pop2 <- df[[input$value]]
+  pop2 <- df$.y
   df$binding <- round(mean(pop2), digits = 0)
   df$n <- length(pop2)
   df$sd <- round(sd(pop2), digits = 0)
@@ -52,10 +79,10 @@ dostats <- function(df, pop1, input) {
   if (dottest) {
     df$p <- ttest(pop1, pop2, input)
   }
-
+  
   if (input$Ignore.negatives == F) {
     if (ctxcore %>% pull(.y) %>% min() <= 0)
-    stop("First, remove zeros and negative values from your measurement!")
+      stop("First, remove zeros and negative values from your measurement!")
     df$ratio <- round(mean(pop2) / mean(pop1), digits = 2)
     df$MI <- round(log((mean(pop2) / mean(pop1)), abase), digits = 2)
   } else {
@@ -65,34 +92,45 @@ dostats <- function(df, pop1, input) {
   return(df)
 }
 
+# select the negative control and apply calculation
 responsefunction <- function(df, input) {
-  NC.factor <- input$NC.factor
-  NC.annot <- input$NC.annotation
-
-  pop1 <- subset(df, df[[NC.factor]] == NC.annot)[[input$value]]
-  df <- df %>% plyr::ddply(NC.factor, function(x) {
+  NC.annot <-  ctx$op.value('negative control') %>% as.character()
+  if(!NCannot %in% ctxcore %>%  pull(NCfactor)){
+    stop("This negative control is not found in the data. Please correct in the operator properties.")
+    
+  }
+  pop1 <- subset(df, NCfactor == NC.annot)$.y
+  
+  df <- df %>% plyr::ddply(~NCfactor, function(x) {
     dostats(x, pop1, input)
-   })
+  })
   return(df)
 }
 
 
+# perform actial calculations for the result
+doper <- c(".ri", "split")
 
-ctxcore  %>% 
-  plyr::ddply(~.ri, function(x) {
+ctxcore <- ctxcore  %>% 
+  plyr::ddply(~split+.ri, function(x){
     responsefunction(x, input) 
-    }) %>%
+  }) %>%
   group_by(.ri, .ci) %>% 
   summarize(
     binding = mean(binding),
     sem = mean(sem),
-    p = mean(p),
     n = mean(n),
     sd = mean(sd),
     cv = mean(cv),
     ratio = mean(ratio),
-    MI = mean(MI) 
-    ) %>% 
+    MI = mean(MI),
+    p = mean(p)
+  ) 
+
+
+ctxcore$pcorr <- p.adjust(ctxcore$p, method='fdr', n=nrow(ctxcore))
+
+ctxcore %>% 
   ctx$addNamespace() %>%
   ctx$save()
   
--- workspace.R
+++ workspace.R
@@ -1,9 +1,12 @@
 library(tercen)
 library(tidyverse)
-# #http://127.0.0.1:5402/#ds/d30382066b71e6e7995cee981c001603/5-6
-# # http://127.0.0.1:5402/#ds/ee0cbc176ba159782635097a88009c5b/8-4
-# options("tercen.workflowId"= "d30382066b71e6e7995cee981c001603")
-# options("tercen.stepId"= "5-6")
+options("tercen.serviceUri"="http://tercen:5400/api/v1/")
+options("tercen.username"="admin")
+options("tercen.password"="admin")
+
+
+options("tercen.workflowId"= "d30382066b71e6e7995cee981c001603")
+options("tercen.stepId"= "33-6")
 
 ctx <- tercenCtx() 
 
@@ -15,12 +18,45 @@ input <- list(
   conf.level = 0.95,
   abase = 10,
   do.ttest = T,
-  NC.factor = ctx$colors[[1]],
-  NC.annotation = "DMSO",
-  value = ".y",
-  Ignore.negatives =  as.logical(ctx$op.value('ignore negatives'))
+  Ignore.negatives = as.logical(ctx$op.value('ignore negatives'))
 )
 
+# extract the data
+ctxcore <- ctx %>%  
+  select() 
+
+# reformat column names for odd characters
+colnames(ctxcore) <- colnames(ctxcore) %>% make.names()
+
+# create a column for negative control annotation
+if(ctx$labels %>% length() <1){
+  stop("Drag the annotation factor for the negative control (NC) to 'Labels' first! \n Next, provide the annotion for the NC in the operator properties.")
+}else{
+  ctxcore$NCfactor = ctxcore[[ctx$labels[[1]] %>% make.names()]]
+}
+
+# create a column to split the data (if present)
+if(ctx$colors %>% length() >0) {
+  asplit <- ctx$colors %>% as.character() 
+  ctxcore <- ctxcore %>% 
+    unite("split" ,asplit, remove=F)
+} else{ctxcore$split = ""} 
+
+# filter for needed data
+ctxcore <- ctxcore %>% 
+  select(.ri, 
+         .ci, 
+         NCfactor,
+         split,
+         .y)
+
+# if indicated in properties, convert negatives values to small positive
+if(as.logical(ctx$op.value('convert negatives'))){
+  ctxcore$.y <- with(ctxcore, replace(.y, .y<1, 1))
+}
+
+  
+# ttest function  
 ttest <- function(pop1, pop2, input) {
   alternative <- input$alternative
   mu <- input$mu
@@ -37,9 +73,10 @@ ttest <- function(pop1, pop2, input) {
   return(p)
 }
 
+# perform some basic calculations
 dostats <- function(df, pop1, input) {
   abase <- input$abase
-  pop2 <- df[[input$value]]
+  pop2 <- df$.y
   df$binding <- round(mean(pop2), digits = 0)
   df$n <- length(pop2)
   df$sd <- round(sd(pop2), digits = 0)
@@ -51,7 +88,7 @@ dostats <- function(df, pop1, input) {
   }
   
   if (input$Ignore.negatives == F) {
-    if (ctx %>% select() %>% pull(.y) %>% min() <= 0)
+    if (ctxcore %>% pull(.y) %>% min() <= 0)
       stop("First, remove zeros and negative values from your measurement!")
     df$ratio <- round(mean(pop2) / mean(pop1), digits = 2)
     df$MI <- round(log((mean(pop2) / mean(pop1)), abase), digits = 2)
@@ -62,23 +99,41 @@ dostats <- function(df, pop1, input) {
   return(df)
 }
 
+# select the negative control and apply calculation
 responsefunction <- function(df, input) {
-  NC.factor <- input$NC.factor
-  NC.annot <- input$NC.annotation
+  NC.annot <-  ctx$op.value('negative control') %>% as.character()
+  if(!NCannot %in% ctxcore %>%  pull(NCfactor)){
+    stop("This negative control is not found in the data. Please correct in the operator properties.")
+    
+  }
+  pop1 <- subset(df, NCfactor == NC.annot)$.y
   
-  pop1 <- subset(df, df[[NC.factor]] == NC.annot)[[input$value]]
-  df <- df %>% plyr::ddply(NC.factor, function(x) {
+  df <- df %>% plyr::ddply(~NCfactor, function(x) {
     dostats(x, pop1, input)
   })
   return(df)
 }
 
-ctx %>%
-  select()  %>% 
-  plyr::ddply(~.ri, function(x) {
-    responsefunction(x, input)
-  }) %>% 
-  ctx$addNamespace() %>%
-  ctx$save()
 
+# perform actial calculations for the result
+doper <- c(".ri", "split")
+
+ctxcore <- ctxcore  %>% 
+  plyr::ddply(~split+.ri, function(x){
+  responsefunction(x, input) 
+  }) %>%
+  group_by(.ri, .ci) %>% 
+  summarize(
+    binding = mean(binding),
+    sem = mean(sem),
+    n = mean(n),
+    sd = mean(sd),
+    cv = mean(cv),
+    ratio = mean(ratio),
+    MI = mean(MI),
+    p = mean(p)
+  ) 
+
+
+ctxcore$pcorr <- p.adjust(ctxcore$p, method='fdr', n=nrow(ctxcore))
 
